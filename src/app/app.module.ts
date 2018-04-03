cb7bd7030a782056ef85c725a4bb0f5be4ab6318
Agnaldo Rogério de Lima Filho <agnaldo.filho@sysmap.com.br>
2018-04-03 14:01:07 -0300

    Version 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AbrirChamadoPage } from '../pages/abrir-chamado/abrir-chamado';
+import { FecharChamadoPage } from '../pages/fechar-chamado/fechar-chamado';
+import { ConsultarChamadoPage } from '../pages/consultar-chamado/consultar-chamado';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    AbrirChamadoPage,
+    FecharChamadoPage,
+    ConsultarChamadoPage,
     TabsPage
   ],
   imports: [
@@ -26,9 +25,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    AbrirChamadoPage,
+    FecharChamadoPage,
+    ConsultarChamadoPage,
     TabsPage
   ],
   providers: [
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
+import { AbrirChamadoPage } from '../abrir-chamado/abrir-chamado';
+import { FecharChamadoPage } from '../fechar-chamado/fechar-chamado';
+import { ConsultarChamadoPage } from '../consultar-chamado/consultar-chamado';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = AbrirChamadoPage;
+  tab2Root = FecharChamadoPage;
+  tab3Root = ConsultarChamadoPage;
 
   constructor() {
 
